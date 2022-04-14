16e3e8e1f9fe8b09905e1f8ef085609bc8e363d0
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-04-14 15:18:53 +0200

    ✏️🔥 Changed typo DockUser -> DockGuest. Remove useless packages at root.

--- frontend/src/components/Dock/DockGuest.tsx
+++ frontend/src/components/Dock/DockGuest.tsx
@@ -7,7 +7,7 @@ import styles from '../../styles/Home.module.css'
 import IconButton from '@mui/material/IconButton';
 import FTLogo from "../../public/42logo.png";
 
-export default function DockUser() {
+export default function DockGuest() {
 	return (
 	  <Dock>
 		<IconButton className={styles.icons} aria-label="Authentification">
