d6a1515f54ecd8e6ae27f6da17f039ba83586e5e
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-04-21 02:17:41 +0200

    ✨✅ Change how the ThemeContext is used. Now you have to import the useThemeContext function instead of importing the ThemeContext and calling useContext. As an example the ToggleDarkMode has been changed to use the new way.

--- frontend/src/context/ThemeContext.tsx
+++ frontend/src/context/ThemeContext.tsx
@@ -10,7 +10,7 @@ const defaultState = {
   dark: false,
 };
 
-export const ThemeContext = React.createContext<IThemeContext>(defaultState);
+const ThemeContext = React.createContext<IThemeContext>(defaultState);
 
 export const ThemeProvider: FC = ({ children }) => {
   const [dark, setDark] = useState(defaultState.dark);
@@ -30,3 +30,7 @@ export const ThemeProvider: FC = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
+
+export function useThemeContext() {
+	return React.useContext(ThemeContext);
+}
--- frontend/src/hooks/ToggleDarkMode.tsx
+++ frontend/src/hooks/ToggleDarkMode.tsx
@@ -1,26 +1,19 @@
-import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import { useThemeContext } from "../context/ThemeContext";
 import React from "react";
 import styles from "../styles/Home.module.css";
 
 export const ToggleDarkMode = () => {
-  const { dark, toggleDark } = useContext(ThemeContext);
+  const themeContext = useThemeContext();
 
   const handleOnClick = () => {
-    toggleDark?.();
-    // console.log(dark);
+    themeContext.toggleDark?.();
   };
 
   return (
     <>
-      {/* <div className={styles.grid}> */}
-      {/* <Link href='/'> */}
       <a className={styles.card} onClick={handleOnClick}>
-        <h2>{dark ? "Dark" : "Light"} </h2>
-        {/* <p>Find in-depth information about Next.js features and API.</p> */}
+        <h2>{themeContext.dark ? "Dark" : "Light"} </h2>
       </a>
-      {/* </Link> */}
-      {/* </div> */}
     </>
   );
 };
