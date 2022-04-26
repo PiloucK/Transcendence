ff598cac39efce5890d12dbbd85c0d3c9bbef7a0
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-04-26 19:55:38 +0200

    🐛✨ I forgot to put an export for the ButtonCreateChannel. Added the logic of accepting a request on the ButtonsFriendRequest.

--- frontend/src/components/Buttons/ButtonCreateChannel.tsx
+++ frontend/src/components/Buttons/ButtonCreateChannel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 
-function ButtonCreateChannel() {
+export function ButtonCreateChannel() {
   return <div className={styles.chat_create_channel_form_create}>Create</div>;
 }
--- frontend/src/components/Buttons/ButtonsFriendRequest.tsx
+++ frontend/src/components/Buttons/ButtonsFriendRequest.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 
 import { IUserPublicInfos } from "../../interfaces/users";
+import { useLoginContext } from "../../context/LoginContext";
+import userServices from "../../services/users";
 
 export function ButtonsFriendRequest({
   userInfos,
 }: {
   userInfos: IUserPublicInfos;
 }) {
+	const loginContext = useLoginContext();
+
+	const acceptFriend = () => {
+		if (loginContext.userLogin !== null && loginContext.userLogin !== userInfos.login42) {
+			userServices.acceptFriendRequest(loginContext.userLogin, userInfos.login42);
+		}
+	}
+
+	const declineFriend = () => {
+		if (loginContext.userLogin !== null && loginContext.userLogin !== userInfos.login42) {
+			// userServices.declineFriendRequest(loginContext.userLogin, userInfos.login42);
+		}
+	}
+
   return (
     <div className={styles.social_friend_request_card_button}>
-      <div className={styles.confirm} onClick={() => {}}>
+      <div className={styles.confirm} onClick={() => {acceptFriend}}>
         Confirm
       </div>
       <div className={styles.decline} onClick={() => {}}>
