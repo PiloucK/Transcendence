d9822f4fa6501d5cd754c7c4a90251c2db1896f9
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-04-27 07:56:30 +0200

    ✨ Adding the list of friend in new direct message if you have friends. (Real time)

--- frontend/src/components/Chat/DirectMessage.tsx
+++ frontend/src/components/Chat/DirectMessage.tsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Home.module.css";
 import { EmptyFriendList } from "../Social/emptyPages";
 
 import { DirectMessageMenu } from "./Menus";
+import { useLoginContext } from "../../context/LoginContext";
+import { IUserPublicInfos } from "../interfaces/users";
+import userService from "../../services/users";
+import { FriendContent } from "../Social/pagesContent";
+
+import io from "socket.io-client";
+
+const socket = io("http://0.0.0.0:3002", { transports: ["websocket"] });
 
 function NewDirectMessage() {
+  const loginContext = useLoginContext();
+  const [friends, setFriends] = useState<IUserPublicInfos[]>([]);
+
+  React.useEffect(() => {
+    userService
+      .getUserFriends(loginContext.userLogin)
+      .then((friends: IUserPublicInfos[]) => {
+        setFriends(friends);
+      });
+
+    socket.on("update-leaderboard", () => {
+      userService
+        .getUserFriends(loginContext.userLogin)
+        .then((friends: IUserPublicInfos[]) => {
+          setFriends(friends);
+        });
+    });
+		socket.on("update-relations", () => {
+      userService
+        .getUserFriends(loginContext.userLogin)
+        .then((friends: IUserPublicInfos[]) => {
+          setFriends(friends);
+        });
+    });
+  }, []);
+
   return (
     <div className={styles.chat_direct_message_content}>
       Select a friend to start a conversation
-      <EmptyFriendList />
+      <FriendContent friends={friends} />
     </div>
   );
 }
