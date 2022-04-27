a31e4a26fe76194221161ce0347d0c1be5a0fd4a
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-04-27 05:31:32 +0200

    ✨🚧 Adding the block|unblock's logic in frontend.

--- frontend/src/components/Buttons/ButtonBlock.tsx
+++ frontend/src/components/Buttons/ButtonBlock.tsx
@@ -3,13 +3,32 @@ import styles from "../../styles/Home.module.css";
 
 import { IUserPublicInfos } from "../../interfaces/users";
 
-export function ButtonBlock({
-  userInfos,
-}: {
-  userInfos: IUserPublicInfos;
-}) {
+import userServices from "../../services/users";
+
+import { useLoginContext } from "../../context/LoginContext";
+
+import io from "socket.io-client";
+
+const socket = io("http://0.0.0.0:3002", { transports: ["websocket"] });
+
+export function ButtonBlock({ userInfos }: { userInfos: IUserPublicInfos }) {
+  const loginContext = useLoginContext();
+
+  const blockAUser = () => {
+    if (
+      loginContext.userLogin !== null &&
+      loginContext.userLogin !== userInfos.login42
+    ) {
+      userServices
+        .blockUser(loginContext.userLogin, userInfos.login42)
+        .then(() => {
+          socket.emit("user:update-relations");
+        });
+    }
+  };
+	
   return (
-    <div className={styles.block_button} onClick={() => {}}>
+    <div className={styles.block_button} onClick={blockAUser}>
       Block
     </div>
   );
