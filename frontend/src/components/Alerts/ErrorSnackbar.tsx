4f0eae8674fce12f2326d0d34351e8db07d6c055
masaki <kumo.git@mailgw.com>
2022-06-01 17:24:11 +0200

    frontend: a click outside of the snackbar doesn't hide it anymore

--- frontend/src/components/Alerts/ErrorSnackbar.tsx
+++ frontend/src/components/Alerts/ErrorSnackbar.tsx
@@ -8,26 +8,35 @@ const CONFLICT = 409;
 const UNKNOWN_ERROR = 520;
 
 export const ErrorSnackbar = () => {
-    const errorContext = useErrorContext();
-    const loginContext = useLoginContext();
+  const errorContext = useErrorContext();
+  const loginContext = useLoginContext();
 
-    const handleOpen = () => {
-        if (errorContext.errorData.statusCode === UNAUTHORIZED) {
-            loginContext.logout?.();
-            // } else if (error.response.status === NOT_FOUND) { ?????
-            // } else if (error.response.status === CONFLICT) { ?????
-        }
-        return (errorContext.showError);
+  const handleOpen = () => {
+    if (errorContext.errorData.statusCode === UNAUTHORIZED) {
+      loginContext.logout?.();
+      // } else if (error.response.status === NOT_FOUND) { ?????
+      // } else if (error.response.status === CONFLICT) { ?????
     }
+    return errorContext.showError;
+  };
 
-    return (
-        <Snackbar
-            open={handleOpen()}
-            autoHideDuration={6000}
-            onClose={errorContext.hideError}
-            message={`Error: ${errorContext.errorData.statusCode} ${errorContext.errorData.error}\n${errorContext.errorData.message}`}
-        // action={action
-        />
-    )
+  const handleClose = (
+    _event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    errorContext.hideError?.();
+  };
 
-}
+  return (
+    <Snackbar
+      open={handleOpen()}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      message={`Error: ${errorContext.errorData.statusCode} ${errorContext.errorData.error}\n${errorContext.errorData.message}`}
+      // action={action
+    />
+  );
+};
--- frontend/src/context/ErrorContext.tsx
+++ frontend/src/context/ErrorContext.tsx
@@ -1,62 +1,62 @@
-import { createContext, useContext, useState } from 'react'
-import React from 'react'
+import { createContext, useContext, useState } from "react";
+import React from "react";
 import getConfig from "next/config";
 
 const { publicRuntimeConfig } = getConfig();
 
 interface IErrorData {
-    error: string;
-    message: string;
-    statusCode: number;
+  error: string;
+  message: string;
+  statusCode: number;
 }
 
 interface IErrorContext {
-    errorData: IErrorData;
-    newError?: (errorData: IErrorData) => void;
-    hideError?: () => void;
-    showError: boolean;
+  errorData: IErrorData;
+  newError?: (errorData: IErrorData) => void;
+  hideError?: () => void;
+  showError: boolean;
 }
 
 const defaultErrorData: IErrorData = {
-    error: 'no error',
-    message: 'no error',
-    statusCode: 0,
+  error: "no error",
+  message: "no error",
+  statusCode: 0,
 };
 
 const defaultErrorState: IErrorContext = {
-    errorData: defaultErrorData,
-    showError: false,
+  errorData: defaultErrorData,
+  showError: false,
 };
 
-const ErrorContext = createContext<IErrorContext>(defaultErrorState)
+const ErrorContext = createContext<IErrorContext>(defaultErrorState);
 
 export const ErrorProvider = ({ children }: { children: React.ReactNode }) => {
-    const [errorData, setErrorData] = useState(defaultErrorState.errorData);
-    const [showError, setShowError] = useState(defaultErrorState.showError);
+  const [errorData, setErrorData] = useState(defaultErrorState.errorData);
+  const [showError, setShowError] = useState(defaultErrorState.showError);
 
-    const newError = (errorData: IErrorData) => {
-        setErrorData(errorData);
-        setShowError(true);
-    }
+  const newError = (errorData: IErrorData) => {
+    setErrorData(errorData);
+    setShowError(true);
+  };
 
-    const hideError = () => {
-        setShowError(false);
-    }
+  const hideError = () => {
+    setShowError(false);
+  };
 
-    return (
-        <ErrorContext.Provider
-            value={{
-                errorData,
-                newError,
-                hideError,
-                showError,
-            }}
-        >
-            {children}
-        </ErrorContext.Provider>
-    )
-}
+  return (
+    <ErrorContext.Provider
+      value={{
+        errorData,
+        newError,
+        hideError,
+        showError,
+      }}
+    >
+      {children}
+    </ErrorContext.Provider>
+  );
+};
 
 export function useErrorContext() {
-    return useContext(ErrorContext)
+  return useContext(ErrorContext);
 }
