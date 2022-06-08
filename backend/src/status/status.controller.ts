23aecd40b6caa93fdca80868231d40da741957a2
masaki <kumo.git@mailgw.com>
2022-06-08 15:57:39 +0200

    backend: status: add routes to add and remove

--- backend/src/status/status.controller.ts
+++ backend/src/status/status.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { StatusDto } from './dto/status.dto';
+import { UserStatus } from './status.entity';
+import { StatusService } from './status.service';
 
 @Controller('status')
-export class StatusController {}
+export class StatusController {
+  constructor(private readonly statusService: StatusService) {}
+
+  @Post()
+  addStatus(@Body() statusDto: StatusDto): Promise<UserStatus> {
+    return this.statusService.add(statusDto);
+  }
+
+  @Delete(':socketid')
+  removeStatus(@Param('socketid') socketId: string): Promise<void> {
+    return this.statusService.remove(socketId);
+  }
+}
--- backend/src/status/status.service.ts
+++ backend/src/status/status.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { StatusDto } from './dto/status.dto';
 import { UserStatus } from './status.entity';
 
 @Injectable()
@@ -9,4 +10,28 @@ export class StatusService {
     @InjectRepository(UserStatus)
     private readonly statusRepository: Repository<UserStatus>,
   ) {}
+
+  async add(statusDto: StatusDto): Promise<UserStatus> {
+    const { socketId } = statusDto;
+
+    const status = this.statusRepository.create({
+      socketId,
+    });
+
+    await this.statusRepository.save(status);
+
+    return status;
+  }
+
+  async remove(socketId: string): Promise<void> {
+    await this.statusRepository.delete(socketId);
+  }
+
+  // findAll(): Promise<UserStatus[]> {
+  //   return this.statusRepository.find();
+  // }
+
+  // findOne(id: string): Promise<UserStatus | void> {
+  //   return this.statusRepository.findOne(id);
+  // }
 }
