90c60ad9ea8707cfb15f084369b9f0e2e0c5a118
masaki <kumo.git@mailgw.com>
2022-06-10 15:17:21 +0200

    backend: websockets: handle client connection
    and disconnection

--- backend/src/websockets/websockets.gateway.ts
+++ backend/src/websockets/websockets.gateway.ts
@@ -1,14 +1,27 @@
 import {
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(3002, { transports: ['websocket'] })
-export class WebsocketsGateway {
+export class WebsocketsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server!: Server;
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log('connection', client.id);
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    console.log('disconnection', client.id);
+  }
 
   @SubscribeMessage('user:new')
   onNewUser() {
