ea8e6c5732c8d9451e8742c97681101adcd6e275
valentinllpz <lugandlopez.valentin@gmail.com>
2022-06-14 22:53:14 +0200

    IA improved with levels

--- frontend/src/components/Game/OpponentPaddle.tsx
+++ frontend/src/components/Game/OpponentPaddle.tsx
@@ -2,22 +2,29 @@ import { useEffect, useRef, useState } from "react";
 import styles from './OpponentPaddle.module.css'
 import { ICoordinates } from "../../interfaces/ICoordinates";
 
+
+const computerLvl = 3; // peut aller de 1 a 3 EASY MEDIUM HARD
+
 const OpponentPaddle = ({ballDirection} : {ballDirection : ICoordinates}) => {
   
   const paddlePosition = useRef(50);
 
   const movePaddle = useRef(true);
 
+  function randomNumberBetween(min : number, max : number) {
+    return Math.random() * (max - min) + min;
+  }
+
   useEffect(() => {
     
     let ballRect = document.getElementById("ball")?.getBoundingClientRect() as DOMRect;
       
-    if (ballRect.x >= window.innerWidth / 2 && ballDirection.x > 0) {
+    if (ballRect.x >= window.innerWidth / 3 && ballDirection.x > 0) {
       const paddleElem = document.getElementById("opponent-paddle") as HTMLElement;
       const ballY = ballRect.y / window.innerHeight * 100;
 
       // if the ball is out of range
-      if (paddlePosition.current - 5 > ballY || paddlePosition.current + 5 < ballY)
+      if (paddlePosition.current - randomNumberBetween(5, 40 - computerLvl * 10) > ballY || paddlePosition.current + randomNumberBetween(5, 40 - computerLvl * 10) < ballY)
         movePaddle.current = true;
 
       if (movePaddle.current) {
