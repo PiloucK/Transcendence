31f7f63fb2137768266c6855aa215b007f637d38
masaki <kumo.git@mailgw.com>
2022-06-15 18:52:35 +0200

    backend: add a new Map for sockets in StatusService
    and refactor "add" function in StatusService

--- backend/src/status/status.service.ts
+++ backend/src/status/status.service.ts
@@ -2,19 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 
 interface StatusMetrics {
-  socketIds: Set<string>;
+  socketCount: number;
   status: 'ONLINE' | 'OFFLINE' | 'IN_GAME' | 'IN_QUEUE';
 }
 
 type Login42 = string;
+type SocketId = string;
 
 @Injectable()
 export class StatusService {
-  constructor(private readonly usersService: UsersService) {}
-
+  private sockets = new Map<SocketId, Login42>();
   private statuses = new Map<Login42, StatusMetrics>();
 
-  add(socketId: string, userLogin42: string): 'EMIT' | 'QUIET' {
+  constructor(private readonly usersService: UsersService) {}
+
+  add(socketId: SocketId, userLogin42: Login42): 'EMIT' | 'QUIET' {
+    this.sockets.set(socketId, userLogin42);
+    const currentUser = this.statuses.get(userLogin42);
+    if (!currentUser) {
+      this.statuses.set(userLogin42, {
+        socketCount: 1,
+        status: 'ONLINE',
+      });
+      return 'EMIT';
+    } else {
+      ++currentUser.socketCount;
+      return 'QUIET';
+    }
+  }
+
+  remove(socketId: string): 'EMIT' | 'QUIET' {
     const currentUser = this.statuses.get(userLogin42);
     if (!currentUser) {
       this.statuses.set(userLogin42, {
@@ -27,29 +44,4 @@ export class StatusService {
       return 'QUIET';
     }
   }
-
-  async remove(socketId: string): Promise<void> {
-    const status = await this.statusRepository.findOne(socketId, {
-      relations: ['user'],
-    });
-    if (status) {
-      const userLogin42 = status.user.login42;
-      await this.statusRepository.remove(status);
-      console.log('remove status');
-      const user = await this.usersService.getUserWithStatus(userLogin42);
-      if (user.status.length === 0) {
-        console.log(user.login42, 'offline');
-        this.usersService.updateOnlineStatus(user.login42, false);
-        this.websocketsGateway.updateRelations();
-      }
-    }
-  }
-
-  // findAll(): Promise<UserStatus[]> {
-  //   return this.statusRepository.find();
-  // }
-
-  // findOne(id: string): Promise<UserStatus | void> {
-  //   return this.statusRepository.findOne(id);
-  // }
 }
