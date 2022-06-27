fda2ad8f2dcf2ad1ccbaa05f2448ae04c8eef060
masaki <kumo.git@mailgw.com>
2022-06-27 19:02:41 +0200

    frontend: change PrivateConv types in Chat.interfaces
    from UserSelf to UserSlim

--- frontend/src/interfaces/Chat.interfaces.ts
+++ frontend/src/interfaces/Chat.interfaces.ts
@@ -3,8 +3,8 @@ import { IUserSelf, IUserSlim } from "./IUser";
 export type Message = { author: string; content: string };
 export type Invitation = { author: string; channelId: string };
 export type PrivateConv = {
-  userOne: IUserSelf;
-  userTwo: IUserSelf;
+  userOne: IUserSlim;
+  userTwo: IUserSlim;
   messages: Array<Message | Invitation>;
 };
 export type restriction = { login: string; until: number }; // login, end of the restriction in ms.
