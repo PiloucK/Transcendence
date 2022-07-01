8f25c5a0bd14444fc673e0dd560ff97afb0e4c73
ldutriez-Inspiron <ldutriez@student.42.fr>
2022-07-01 20:36:22 +0200

    💄✨ Now when you login for the first time, you'll have to set your profile. This component has been styled and you can now set your avatar.

--- frontend/src/components/Alerts/SetUsername.tsx
+++ frontend/src/components/Alerts/SetUsername.tsx
@@ -7,12 +7,26 @@ import { useSessionContext } from "../../context/SessionContext";
 import { errorHandler } from "../../errors/errorHandler";
 import userService from "../../services/user";
 import { TextField } from "../Inputs/TextField";
+import Avatar from "@mui/material/Avatar";
+import { styled } from "@mui/material/styles";
 
 export const SetUsernameDialog = () => {
   const sessionContext = useSessionContext();
   const errorContext = useErrorContext();
   const [username, setUsername] = useState("");
   const [textFieldError, setTextFieldError] = useState("");
+
+  const [newImage, setNewImage] = useState<Blob>();
+  const [preview, setPreview] = useState("");
+
+  const updateNewImage = (event) => {
+    setNewImage(event.target.files[0]);
+    setPreview(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const Input = styled("input")({
+    display: "none",
+  });
 
   const updateUsername: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
@@ -23,6 +37,19 @@ export const SetUsernameDialog = () => {
       userService
         .updateUserUsername(sessionContext.userSelf.login42, username)
         .then(() => {
+          if (newImage !== undefined) {
+            const formData = new FormData();
+            formData.append("file", newImage);
+            userService
+              .updateUserImage(sessionContext.userSelf.login42, formData)
+              .then(() => {
+                setNewImage(undefined);
+                setPreview("");
+              })
+              .catch((error) => {
+                errorContext.newError?.(errorHandler(error, sessionContext));
+              });
+          }
           sessionContext.updateUserSelf?.();
         })
         .catch((caughtError: Error | AxiosError) => {
@@ -42,8 +69,19 @@ export const SetUsernameDialog = () => {
   };
 
   return (
-    <Dialog open={!sessionContext.userSelf.username}>
-      <DialogTitle>Choose a username</DialogTitle>
+    <Dialog
+      PaperProps={{
+        style: {
+          backgroundColor: "#163F5B",
+          minWidth: "350px",
+          minHeight: "350px",
+          display: "grid",
+          justifyContent: "center",
+        },
+      }}
+      open={!sessionContext.userSelf.username}
+    >
+      <DialogTitle>Set your profile</DialogTitle>
       <form onSubmit={updateUsername}>
         <TextField
           value={username}
@@ -52,6 +90,49 @@ export const SetUsernameDialog = () => {
         />
         <Button type="submit">ok</Button>
       </form>
+      <label htmlFor="icon-button-file">
+        <Input
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+          onChange={updateNewImage}
+        />
+        <Avatar
+          src={preview}
+          alt="avatar"
+          sx={{
+            left: "50%",
+            transform: "translateX(-50%)",
+            width: 151,
+            height: 151,
+            cursor: "pointer",
+          }}
+        >
+          <Avatar
+            src={sessionContext.userSelf.image}
+            alt="avatar"
+            sx={{
+              left: "50%",
+              transform: "translateX(-50%)",
+              width: 151,
+              height: 151,
+              cursor: "pointer",
+            }}
+          >
+            <Avatar
+              src={sessionContext.userSelf.photo42}
+              alt="avatar"
+              sx={{
+                left: "50%",
+                transform: "translateX(-50%)",
+                width: 151,
+                height: 151,
+                cursor: "pointer",
+              }}
+            />
+          </Avatar>
+        </Avatar>
+      </label>
     </Dialog>
   );
 };
