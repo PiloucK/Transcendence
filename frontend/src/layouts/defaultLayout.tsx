c355368590ca8ed2c0954e9f67481c62a58e2b82
Lucas DUTRIEZ <ldutriez@e1r9p4.clusters.42paris.fr>
2022-07-06 14:58:37 +0200

    ✨🚸 Add logic to easily go to index. The first time the user connect, a text will the feature.

--- frontend/src/layouts/defaultLayout.tsx
+++ frontend/src/layouts/defaultLayout.tsx
@@ -1,53 +1,25 @@
-import React, { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
-import { DockSelector } from "../components/Dock/DockSelector";
+import React, { useEffect } from "react";
 import { useSessionContext } from "../context/SessionContext";
 import { useErrorContext } from "../context/ErrorContext";
 import { useSocketContext } from "../context/SocketContext";
 import { authenticate } from "../events/authenticate";
-import { showOverlayOnEscape } from "../events/showOverlayOnEscape";
 import { GameInvitation } from "../components/Cards/GameInvitation";
-import { useUserStatusContext } from "../context/UserStatusContext";
+import { goToIndexOnEscape } from "../events/goToIndexOnEscape";
 
 export const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
-  const sessionContext = useSessionContext();
-  const errorContext = useErrorContext();
-  const socketContext = useSocketContext();
-  const userStatusContext = useUserStatusContext();
+	const sessionContext = useSessionContext();
+	const errorContext = useErrorContext();
+	const socketContext = useSocketContext();
 
-  useEffect(() => {
-    authenticate(errorContext, socketContext, sessionContext);
-  }, []);
+	useEffect(() => {
+		window.addEventListener('keydown', goToIndexOnEscape, true);
+		authenticate(errorContext, socketContext, sessionContext);
+	}, []);
 
-  const [showOverlay, setShowOverlay] = useState(false);
-
-  const findMatch = () => {
-    socketContext.socket.emit(
-      "user:find-match",
-      sessionContext.userSelf.login42
-    );
-  };
-  // showOverlayOnEscape(showOverlay, setShowOverlay);
-
-  // have to always show children and overlay if the is activated !! lags come from rerendering all components in the page
-  return (
-    <>
-      {/* {showOverlay ? (
-        <>
-          <div className={styles.mainLayout_left_background} />
-          <div className={styles.mainLayout_right_background} />
-          {userStatusContext.statuses.get(sessionContext.userSelf.login42)
-            ?.status === "ONLINE" && (
-            <div className={styles.play} onClick={findMatch}>
-              PLAY
-            </div>
-          )}
-          <DockSelector />
-        </>
-      ) : ( */}
-		<GameInvitation />
-        {children}
-    {/*   )} */}
-    </>
-  );
+	return (
+		<>
+			<GameInvitation />
+			{children}
+		</>
+	);
 };
